dee7ccdb3f54b7370cff5ce74d35ca20ad18bc1f
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-09-25 17:29:08 -0400

    NetworkManager: model scaffold, field extractions and `Result<T>` wrapper for UI

--- netrs-core/src/models.rs
+++ netrs-core/src/models.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -6,6 +6,18 @@ pub struct Network {
     pub ssid: String,
     pub strength: u8,
     pub secure: bool,
+}
+
+// Abstracting way the bus allows us to dump devices into the UI
+// without worrying about OwnedObjectPath, zbus::Connection, D-Bus errors, etc.
+#[derive(Debug, Clone)]
+pub struct Device {
+    pub path: String,
+    pub interface: String,
+    pub device_type: u32,
+    pub state: u32,
+    pub managed: Option<bool>,
+    pub driver: Option<String>,
 }
 
 #[derive(Debug, Error)]
