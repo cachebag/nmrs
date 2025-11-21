3c528a7ed00a1a11d74dcdbe5ab16d697b315191
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-11-21 17:25:08 -0500

    feat: (#54) allow user to override styles

--- nmrs-ui/src/style.rs
+++ nmrs-ui/src/style.rs
@@ -1,6 +1,25 @@
-use gtk::CssProvider;
-use gtk::STYLE_PROVIDER_PRIORITY_APPLICATION;
 use gtk::gdk::Display;
+use gtk::gio::File;
+use gtk::{CssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION, STYLE_PROVIDER_PRIORITY_USER};
+
+fn load_user_css_if_exists(display: &Display) {
+    let path = dirs::config_dir()
+        .unwrap_or_default()
+        .join("nmrs/style.css");
+
+    if path.exists() {
+        let provider = CssProvider::new();
+        let file = File::for_path(&path);
+
+        provider.load_from_file(&file);
+
+        gtk::style_context_add_provider_for_display(
+            display,
+            &provider,
+            STYLE_PROVIDER_PRIORITY_USER,
+        );
+    }
+}
 
 pub fn load_css() {
     let provider = CssProvider::new();
@@ -9,9 +28,12 @@ pub fn load_css() {
     provider.load_from_data(css);
 
     let display = Display::default().expect("No display found");
+
     gtk::style_context_add_provider_for_display(
         &display,
         &provider,
         STYLE_PROVIDER_PRIORITY_APPLICATION,
     );
+
+    load_user_css_if_exists(&display);
 }
