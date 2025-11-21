4f0601e200af4121f385fc70e14186f1915137c8
joncorv <joncorv@gmail.com>
2025-11-20 23:55:55 -0800

    Local nixpkgs derivation added using buildRustPackage

---
+++ package.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  glib-networking,
+  pkg-config,
+  wrapGAppsHook4,
+  libxkbcommon,
+  wayland,
+  glib,
+  gobject-introspection,
+  gtk4,
+  libadwaita,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "nmrs";
+  version = "0.1.0-beta";
+
+  src = ./.;
+
+  cargoHash = "sha256-GmTCBRhr5M1bvggzcSUUcR7RZ2bGniv8Yd01W5ZdbLM=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [ wrapGAppsHook4 ];
+
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
+    glib-networking
+    libxkbcommon
+    wayland
+    glib
+    gobject-introspection
+    gtk4
+    libadwaita
+  ];
+
+  doCheck = false;
+  doInstallCheck = true;
+
+  meta = with lib; {
+    description = "Wayland-native frontend for NetworkManager. ";
+    homepage = "https://github.com/cachebag/nmrs";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "nmrs";
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
