66a640e33bd1baa6743ff85677f5a277f568ea6f
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-12-20 01:06:21 -0500

    chore: update nix flake

--- package.nix
+++ package.nix
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage {
 
   src = ./.;
 
-  cargoHash = "sha256-uvH7OtKT0laBXyfv70rV5XQKPHZBmRmr1p2y9Wxom7E=";
+  cargoHash = "sha256-hmkUlcw/D38ASqo7GcNK8p6IqfnFIAQWRrjpWymzKFY=";
 
   nativeBuildInputs = [
     pkg-config
