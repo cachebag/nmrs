c4ceb73141995e23b305d4e95a381d2ac3a6307f
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2026-03-27 17:22:15 -0400

    refactor(nmrs): refactor parser into sub-module + error shape

---
+++ nmrs/src/core/ovpn_parser/error.rs
@@ -0,0 +1,65 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub enum OvpnParseError {
+    InvalidDirectiveSyntax {
+        line: usize,
+    },
+    InvalidArgument {
+        key: String,
+        arg: String,
+        line: usize,
+    },
+    InvalidContinuation {
+        line: usize,
+    },
+    UnterminatedBlock {
+        block: String,
+        line: usize,
+    },
+    UnexpectedBlockEnd {
+        block: String,
+        line: usize,
+    },
+    UnexpectedEof {
+        line: usize,
+    },
+    InvalidNumber {
+        key: String,
+        value: String,
+        line: usize,
+    },
+}
+
+impl fmt::Display for OvpnParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            OvpnParseError::InvalidDirectiveSyntax { line } => {
+                write!(f, "invalid directive syntax at line {line}")
+            }
+            OvpnParseError::InvalidArgument { key, arg, line } => {
+                write!(
+                    f,
+                    "invalid argument '{arg}' for directive '{key}' at line {line}"
+                )
+            }
+            OvpnParseError::InvalidContinuation { line } => {
+                write!(f, "invalid continuation at line {line}")
+            }
+            OvpnParseError::UnterminatedBlock { block, line } => {
+                write!(f, "unterminated block '{block}' starting at line {line}")
+            }
+            OvpnParseError::UnexpectedBlockEnd { block, line } => {
+                write!(f, "unexpected end of block '{block}' at line {line}")
+            }
+            OvpnParseError::UnexpectedEof { line } => {
+                write!(f, "unexpected EOF at line {line}")
+            }
+            OvpnParseError::InvalidNumber { key, value, line } => {
+                write!(f, "invalid value '{value}' for '{key}' at line {line}")
+            }
+        }
+    }
+}
+
+impl std::error::Error for OvpnParseError {}
