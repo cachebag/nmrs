2c1072f8e9b641b3193bc3c294cf0a840de9d02e
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2026-03-31 15:53:02 -0400

    feat(nmrs): `.ovpn` parser

--- nmrs/src/core/ovpn_parser/parser.rs
+++ nmrs/src/core/ovpn_parser/parser.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::net::Ipv4Addr;
 
 use crate::api::models::ConnectionError;
 use crate::core::ovpn_parser::error::OvpnParseError;
@@ -54,7 +55,11 @@ pub struct OvpnFile {
     pub auth: Option<String>,
 
     // compress directive. Either enabled or specifies algorithm (e.g. "lz4").
-    pub compress: Option<Compress>,
+    pub compress: Option<String>,
+
+    // OpenVPN 2.5+ specifies a allow-compress directive for safety
+    // https://community.openvpn.net/Security%20Announcements/VORACLE
+    pub allow_compress: Option<AllowCompress>,
 
     // All route directives.
     // Each represents a network route pushed or defined locally.
@@ -62,7 +67,7 @@ pub struct OvpnFile {
 
     // redirect-gateway flag.
     // Forces all traffic through VPN if present.
-    pub redirect_gateway: bool,
+    pub redirect_gateway: Option<RedirectGateway>,
 
     // Standalone flag directives with no arguments.
     // Examples: client, nobind, persist-key, persist-tun.
@@ -94,16 +99,27 @@ pub struct TlsAuth {
 }
 
 #[derive(Debug, Clone)]
-pub enum Compress {
-    Enabled,
-    Algorithm(String),
+pub enum AllowCompress {
+    Yes,
+    No,
+    Asym,
+    Other(String),
 }
 
 #[derive(Debug, Clone)]
 pub struct Route {
-    pub network: String,
-    pub netmask: Option<String>,
-    pub gateway: Option<String>,
+    pub network: Ipv4Addr,
+    pub netmask: Option<Ipv4Addr>,
+    pub gateway: Option<Ipv4Addr>,
+}
+
+#[derive(Debug, Clone)]
+pub struct RedirectGateway {
+    pub def1: bool,
+    pub bypass_dhcp: bool,
+    pub bypass_dns: bool,
+    pub local: bool,
+    pub ipv6: bool,
 }
 
 enum OvpnItem {
@@ -246,5 +262,266 @@ fn lexer(input: &str) -> Result<Vec<OvpnItem>, OvpnParseError> {
 }
 
 pub fn parse_ovpn(content: &str) -> Result<OvpnFile, ConnectionError> {
-    todo!()
+    let mut remotes: Vec<Remote> = Vec::new();
+    let mut dev: Option<String> = None;
+    let mut proto: Option<String> = None;
+    let mut ca: Option<CertSource> = None;
+    let mut cert: Option<CertSource> = None;
+    let mut key: Option<CertSource> = None;
+    let mut tls_auth: Option<TlsAuth> = None;
+    let mut tls_crypt: Option<CertSource> = None;
+    let mut cipher: Option<String> = None;
+    let mut data_ciphers: Vec<String> = Vec::new();
+    let mut auth: Option<String> = None;
+    let mut compress: Option<String> = None;
+    let mut allow_compress: Option<AllowCompress> = None;
+    let mut routes: Vec<Route> = Vec::new();
+    let mut redirect_gateway: Option<RedirectGateway> = None;
+    let mut flags: Vec<String> = Vec::new();
+    let mut options: HashMap<String, Vec<String>> = HashMap::new();
+
+    let items = lexer(content)?;
+
+    for item in items {
+        match item {
+            OvpnItem::Directive { key, args } => {
+                match key.as_str() {
+                    "remote" => {
+                        // remote <HOST> [PORT] [PROTO]
+
+                        let host = args
+                            .get(0)
+                            .ok_or(OvpnParseError::InvalidArgument {
+                                key: key.clone(),
+                                // FIXME: Add `MissingArgument` variant to allow for
+                                // omitting args
+                                arg: "".into(),
+                                line: 0, // FIXME: track lines
+                            })?
+                            .clone();
+
+                        let port = args.get(1).and_then(|p| p.parse::<u16>().ok()); // FIXME: ehh...
+
+                        let proto = args.get(2).cloned();
+
+                        remotes.push(Remote { host, port, proto });
+                    }
+                    "dev" => {
+                        // dev <DEVICE>
+
+                        if args.len() != 1 {
+                            return Err(OvpnParseError::InvalidArgument {
+                                key: key.clone(),
+                                arg: format!("{args:?}"),
+                                line: 0,
+                            })?;
+                        }
+
+                        let value = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        dev = Some(value.clone());
+                    }
+                    "proto" => {
+                        // proto <PROTOCOL>
+
+                        let value = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        proto = Some(value.clone());
+                    }
+                    "cipher" => {
+                        // cipher <CIPHER>
+                        // Note: This is deprecated in new configs
+
+                        let value = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        cipher = Some(value.clone());
+                    }
+                    "data-ciphers" => {
+                        // data-ciphers <[cipher1]:[cipher2]...>
+
+                        let ciphers = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        data_ciphers.extend(ciphers.split(':').map(String::from));
+                    }
+                    "auth" => {
+                        // auth <ALGORITHM>
+
+                        let value = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        auth = Some(value.clone());
+                    }
+                    "compress" => {
+                        // compress <ALGORITHM>
+
+                        let value = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        compress = Some(value.clone());
+                    }
+                    "allow-compress" => {
+                        // allow-compress asym (default) <- receive compressed data but don't send
+                        // allow-compress [yes/no]
+
+                        let value = args.get(0).ok_or(OvpnParseError::InvalidArgument {
+                            key,
+                            arg: "".into(),
+                            line: 0,
+                        })?;
+
+                        let parsed = match value.as_str() {
+                            "yes" => AllowCompress::Yes,
+                            "no" => AllowCompress::No,
+                            "asym" => AllowCompress::Asym,
+                            other => AllowCompress::Other(other.to_string()),
+                        };
+
+                        allow_compress = Some(parsed);
+                    }
+                    "route" => {
+                        // route <NETWORK> [NETMASK] [GATEWAY]
+
+                        let network = parse_ipv4_arg(&key, args.get(0), 0)?;
+                        let netmask = args
+                            .get(1)
+                            .map(|v| parse_ipv4_arg(&key, Some(v), 0))
+                            .transpose()?;
+                        let gateway = args
+                            .get(2)
+                            .map(|v| parse_ipv4_arg(&key, Some(v), 0))
+                            .transpose()?;
+
+                        routes.push(Route {
+                            network,
+                            netmask,
+                            gateway,
+                        });
+                    }
+                    "redirect-gateway" => {
+                        let mut rg = RedirectGateway {
+                            def1: false,
+                            bypass_dhcp: false,
+                            bypass_dns: false,
+                            local: false,
+                            ipv6: false,
+                        };
+
+                        for arg in args {
+                            match arg.as_str() {
+                                "def1" => rg.def1 = true,
+                                "bypass-dhcp" => rg.bypass_dhcp = true,
+                                "bypass-dns" => rg.bypass_dns = true,
+                                "local" => rg.local = true,
+                                "ipv6" => rg.ipv6 = true,
+                                _ => {}
+                            }
+                        }
+
+                        redirect_gateway = Some(rg);
+                    }
+                    _ => {
+                        if args.is_empty() {
+                            flags.push(key);
+                        } else {
+                            options.entry(key).or_default().extend(args);
+                        }
+                    }
+                }
+            }
+            OvpnItem::Block {
+                key: block_key,
+                content,
+            } => {
+                match block_key.as_str() {
+                    "ca" => {
+                        ca = Some(CertSource::Inline(content));
+                    }
+
+                    "cert" => {
+                        cert = Some(CertSource::Inline(content));
+                    }
+
+                    "key" => {
+                        key = Some(CertSource::Inline(content));
+                    }
+
+                    "tls-auth" => {
+                        tls_auth = Some(TlsAuth {
+                            source: CertSource::Inline(content),
+                            key_direction: None, // FIXME: handle seperately
+                        });
+                    }
+
+                    "tls-crypt" => {
+                        tls_crypt = Some(CertSource::Inline(content));
+                    }
+
+                    _ => {
+                        options.entry(block_key).or_default().push(content);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(OvpnFile {
+        remotes,
+        dev,
+        proto,
+        ca,
+        cert,
+        key,
+        tls_auth,
+        tls_crypt,
+        cipher,
+        data_ciphers,
+        auth,
+        compress,
+        allow_compress,
+        routes,
+        redirect_gateway,
+        flags,
+        options,
+    })
+}
+
+fn parse_ipv4_arg(
+    key: &str,
+    value: Option<&String>,
+    line: usize,
+) -> Result<Ipv4Addr, OvpnParseError> {
+    let v = value.ok_or(OvpnParseError::InvalidArgument {
+        key: key.to_string(),
+        arg: "".into(),
+        line,
+    })?;
+
+    v.parse::<Ipv4Addr>()
+        .map_err(|_| OvpnParseError::InvalidNumber {
+            key: key.to_string(),
+            value: v.clone(),
+            line,
+        })
 }
