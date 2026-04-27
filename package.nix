f4ae478adcdabd897d265f565f48c2921db0c17a
akrm <alk9fh@bosch.com>
2026-04-27 09:51:30 -0400

    Revert "fix(nix): update cargoHash"
    This reverts commit f06e446e4f26019ffa587c496e7df425e178d3ff.

--- package.nix
+++ package.nix
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage {
 
   src = ./.;
 
-  cargoHash = "sha256-KZ2MQjbXmy41HGpte8n/WC2EgvLQ68EqDS46E4j067A=";
+  cargoHash = "sha256-ppcOdCsxscwQVFOxJpXex3WZ/ucAdBhkyqpq09IQ+R4=";
 
   nativeBuildInputs = [
     pkg-config
